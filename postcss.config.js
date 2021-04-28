214c1af062e5315605c09fe38bae29813c639239
billy4479 <giachi.ellero@gmail.com>
2021-04-28 19:45:14 +0200

    Removed double CSS pruning.

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./src/**/*.html'],
-  css: ['./src/**/*.css'],
-});
 
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('precss'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-  ],
+  plugins: [require('tailwindcss'), require('autoprefixer'), require('precss')],
 };
