8d9441c71c70222adb40d9c5895348495aab86d0
billy4479 <giachi.ellero@gmail.com>
2021-05-17 11:34:31 +0200

    Fixed styles in production.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.css'],
+  purge: {
+    content: ['./src/**/*.html', './src/**/*.css'],
+
+    options: {
+      safelist: ['text-gray-900', 'dark:bg-gray-900', 'dark:text-gray-100'],
+    },
+  },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
